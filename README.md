af33a7e2f91a4dcef8334c64bb3f5852f990b5a1
Bilawal Hameed <bilawal@studenthack.com>
2015-04-28 17:58:15 +0100

    v1

---
+++ README.md
@@ -0,0 +1,31 @@
+# Lightweight Swift Cache
+
+Just literally something I threw up. I think you'll like it because it is so simple to work with (and tested on XCode 6.3.1 and Swift 1.2)
+
+### Installation
+
+Just copy the contents of `SwiftCache.swift` and insert it into your XCode. Nothing else necessary.
+
+### Example
+
+```swift
+
+var names = SwiftCache(name: "friendsNames", expiresIn: 60)
+// The above will save it for 60 seconds before expiring the cache
+
+// Want to clear the cache? Run this.
+// names.clearCache()
+
+names.respond { data in
+    if names.isCached() {
+        println("Saved in cache: \(data)")
+        println("Expires from cache in \(names.secondsLeftInCache()) seconds")
+    } else {
+        println("Written to cache: \(data)")
+    }
+}.request { _ in
+  // Saved to cache
+    names.saveToCache(["Mike", "Jon", "Aziz", "Tim", "Joe", "Syeef", "Hamer", "Li", "Gregor"])
+}
+
+```
---
+++ SwiftCache.swift
@@ -0,0 +1,98 @@
+//
+//  SwiftCache.swift
+//  MLH
+//
+//  Created by Bilawal Hameed on 28/04/2015.
+//  Copyright (c) 2015 Major League Hacking. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class SwiftCache {
+    var name : String
+    var altName : String
+    var expiresIn : Int
+    var data : AnyObject
+    var respond : (AnyObject) -> Void
+    
+    init(name : String, expiresIn: Int) {
+        self.name = name
+        self.altName = "\(name)-expires"
+        self.expiresIn = expiresIn
+        self.data = [[String: AnyObject]]()
+        self.respond = { objectData in }
+    }
+    
+    func request(callback: (SwiftCache) -> Void) -> SwiftCache {
+        if isCached() {
+            self.respond(gets())
+        } else {
+            callback(self)
+        }
+        
+        return self
+    }
+    
+    func respond(callback: (AnyObject) -> Void) -> SwiftCache {
+        self.respond = callback
+        return self
+    }
+    
+    func expireCache() {
+        NSUserDefaults().removeObjectForKey(self.altName)
+        NSUserDefaults().removeObjectForKey(self.name)
+    }
+    
+    private func gets() -> AnyObject {
+        return NSUserDefaults().objectForKey(self.name)!
+    }
+    
+    func saveToCache(json: [JSON]) -> SwiftCache {
+        var convertedToObj = [[String : AnyObject]]()
+        
+        for (obj : JSON) in json {
+            convertedToObj.append(obj.dictionaryObject!)
+        }
+        
+        return saveToCache(convertedToObj)
+    }
+    
+    func saveToCache(data: [[String: AnyObject]]) -> SwiftCache {
+        return saveToCache(data)
+    }
+    
+    func saveToCache(data: AnyObject) -> SwiftCache {
+        self.data = data
+        self.respond(data)
+        
+        let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+            // do some task
+//            dispatch_async(dispatch_get_main_queue()) {
+//                // update some UI
+//            }
+            self.save()
+        }
+        
+        return self
+    }
+    
+    private func save() {
+        // Save to cache
+        NSUserDefaults().setObject(Int(NSDate().timeIntervalSince1970) + self.expiresIn, forKey: self.altName)
+        NSUserDefaults().setObject(data, forKey: self.name)
+    }
+    
+    func secondsLeftInCache() -> Int {
+        if let cachedUntil = NSUserDefaults().integerForKey(self.altName) as? Int {
+            return cachedUntil - Int(NSDate().timeIntervalSince1970)
+        } else {
+            return -1
+        }
+    }
+    
+    func isCached() -> Bool {
+        return secondsLeftInCache() > 0
+    }
+}
